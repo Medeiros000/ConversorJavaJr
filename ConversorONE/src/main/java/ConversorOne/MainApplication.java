eff2743bc77e794149487241ec63f89862dfe529
Medeiros000 <98979459+Medeiros000@users.noreply.github.com>
2023-03-22 20:04:51 -0300

    Commit #2

--- ConversorONE/src/main/java/ConversorOne/MainApplication.java
+++ ConversorONE/src/main/java/ConversorOne/MainApplication.java
@@ -3,10 +3,11 @@ package ConversorOne;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
-
 import java.io.IOException;
+import java.util.Objects;
 
 public class MainApplication extends Application {
 
@@ -20,7 +21,7 @@ public class MainApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("Main-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 350, 500);
+        Scene scene = new Scene(fxmlLoader.load());
         stage.initStyle(StageStyle.TRANSPARENT);
         scene.setOnMousePressed(event -> {
             xOffset = event.getSceneX();
@@ -31,6 +32,8 @@ public class MainApplication extends Application {
             stage.setY(event.getScreenY() - yOffset);
         });
         stage.setScene(scene);
+        stage.getIcons().add(new Image(Objects.requireNonNull(getClass().getResourceAsStream("/icons/vector.png"))));
+
         stage.show();
     }
 }
